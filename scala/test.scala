d07a9a6b0b4ee6f12a94d499b11833efb2839a05
rupii <mudavathrupesh@gmail.com>
2018-10-07 17:30:09 +0530

    scala

--- scala/test.scala
+++ scala/test.scala
@@ -1,1 +1,16 @@
-println(args)
+// args.foreach(println)
+
+args.foreach(arg=> println(arg))
+
+// for(arg <- args){
+//     println(arg)
+// }
+//Map
+// var m = Map("2"->2)
+// m += "b"-> 3
+// println(m)
+
+//lambda function
+// val x = (m:Int)=> m/2
+//
+// var x: HashMap[Int, String] = new HashMap[Int, String]()
