a04134858036161a248678df1bb2b092836aa19d
rupii <mudavathrupesh@gmail.com>
2018-12-11 17:53:27 +0530

    spark :wq

--- scala/prime.scala
+++ scala/prime.scala
@@ -1,9 +1,9 @@
 def isPrime(a:Int):Boolean ={
     for(i <- Range(2, a)){
         if(a%i == 0)
-         return false
+          false
     }
-    return true
+     true
 }
 for (i <- 2 to 30){
     if (isPrime(i))
